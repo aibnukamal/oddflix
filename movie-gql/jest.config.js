cc7b2fc360826f2910984729ea6f680b7b8966b2
aibnukamal <aibnukamal@gmail.com>
2022-08-01 11:53:22 +0700

    feat(workflow): update

--- movie-gql/jest.config.js
+++ movie-gql/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
+  reporters: ["default", "jest-junit"],
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
